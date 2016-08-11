35647564da0cffe75a89f655ee46c64be4f031be
johnny saylor <jsaylor@saylor-03.feldberg.brandeis.edu>
2016-08-11 14:01:17 -0400

    from raw text to YAML: ok
    now i can work on editing raw YAML

--- generate-yaml.py
+++ generate-yaml.py
@@ -2,43 +2,140 @@
 # reads raw-analects.txt
 # creates YAML output
 
+import pprint
 import re
 import yaml
 
-## SUBROUTINES
+## ROMAN INT CONVERSION
+# from http://code.activestate.com/recipes/81611-roman-numerals/
 
-# conversion routines from https://code.activestate.com/recipes/81611-roman-numerals/
-numeral_map = tuple(zip(
-    (1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1),
-    ('M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I')
-))
+def int_to_roman(input):
+   """
+   Convert an integer to Roman numerals.
 
-def int_to_roman(i):
-    result = []
-    for integer, numeral in numeral_map:
-        count = i // integer
-        result.append(numeral * count)
-        i -= integer * count
-        return ''.join(result)
+   Examples:
+   >>> int_to_roman(0)
+   Traceback (most recent call last):
+   ValueError: Argument must be between 1 and 3999
 
-def roman_to_int(n):
-    i = result = 0
-    for integer, numeral in numeral_map:
-        while n[i:i + len(numeral)] == numeral:
-            result += integer
-            i += len(numeral)
-            return result
+   >>> int_to_roman(-1)
+   Traceback (most recent call last):
+   ValueError: Argument must be between 1 and 3999
+
+   >>> int_to_roman(1.5)
+   Traceback (most recent call last):
+   TypeError: expected integer, got <type 'float'>
+
+   >>> for i in range(1, 21): print int_to_roman(i)
+   ...
+   I
+   II
+   III
+   IV
+   V
+   VI
+   VII
+   VIII
+   IX
+   X
+   XI
+   XII
+   XIII
+   XIV
+   XV
+   XVI
+   XVII
+   XVIII
+   XIX
+   XX
+   >>> print int_to_roman(2000)
+   MM
+   >>> print int_to_roman(1999)
+   MCMXCIX
+   """
+   if type(input) != type(1):
+      raise TypeError, "expected integer, got %s" % type(input)
+   if not 0 < input < 4000:
+      raise ValueError, "Argument must be between 1 and 3999"   
+   ints = (1000, 900,  500, 400, 100,  90, 50,  40, 10,  9,   5,  4,   1)
+   nums = ('M',  'CM', 'D', 'CD','C', 'XC','L','XL','X','IX','V','IV','I')
+   result = ""
+   for i in range(len(ints)):
+      count = int(input / ints[i])
+      result += nums[i] * count
+      input -= ints[i] * count
+   return result
+
+
+
+def roman_to_int(input):
+   """
+   Convert a roman numeral to an integer.
+   
+   >>> r = range(1, 4000)
+   >>> nums = [int_to_roman(i) for i in r]
+   >>> ints = [roman_to_int(n) for n in nums]
+   >>> print r == ints
+   1
+
+   >>> roman_to_int('VVVIV')
+   Traceback (most recent call last):
+    ...
+   ValueError: input is not a valid roman numeral: VVVIV
+   >>> roman_to_int(1)
+   Traceback (most recent call last):
+    ...
+   TypeError: expected string, got <type 'int'>
+   >>> roman_to_int('a')
+   Traceback (most recent call last):
+    ...
+   ValueError: input is not a valid roman numeral: A
+   >>> roman_to_int('IL')
+   Traceback (most recent call last):
+    ...
+   ValueError: input is not a valid roman numeral: IL
+   """
+   if type(input) != type(""):
+      raise TypeError, "expected string, got %s" % type(input)
+   input = input.upper()
+   nums = ['M', 'D', 'C', 'L', 'X', 'V', 'I']
+   ints = [1000, 500, 100, 50,  10,  5,   1]
+   places = []
+   for c in input:
+      if not c in nums:
+         raise ValueError, "input is not a valid roman numeral: %s" % input
+   for i in range(len(input)):
+      c = input[i]
+      value = ints[nums.index(c)]
+      # If the next place holds a larger number, this value is negative.
+      try:
+         nextvalue = ints[nums.index(input[i +1])]
+         if nextvalue > value:
+            value *= -1
+      except IndexError:
+         # there is no next place.
+         pass
+      places.append(value)
+   sum = 0
+   for n in places: sum += n
+   # Easiest test for validity...
+   if int_to_roman(sum) == input:
+      return sum
+   else:
+      raise ValueError, 'input is not a valid roman numeral: %s' % input
+
 
 ## MAIN
 f = open( 'raw-analects.txt', 'r' )
 
+pp = pprint.PrettyPrinter( indent = 2 )
 analects = []
 currentBook = { 'bookChapters' : [] }
 currentChapter = { 'chapterVerses' : [] }
 nextLineTitle = False
 
 for line in f:
-    
+
     # skip blanks
     m = re.match( '^\s*$', line )
     if m:
@@ -53,8 +150,10 @@ for line in f:
     else:
         m = re.match( '^BOOK (\w+)\.', l )
         if m:
-            size = len( currentBook )
-            if size > 0:
+            if 'bookNumber' in currentBook:
+                # clean up old chapter
+                currentBook['bookChapters'].append( currentChapter )
+                currentChapter = { 'chapterVerses' : [] }
                 analects.append( currentBook )
                 currentBook = { 'bookChapters' : [] }
             r = m.group( 1 )
@@ -64,8 +163,7 @@ for line in f:
 
         m = re.match( '^CHAP\. (\w+)\.', l )
         if m:
-            size = len( currentChapter )
-            if size > 0:
+            if 'chapterNumber' in currentChapter:
                 currentBook['bookChapters'].append( currentChapter )
                 currentChapter = { 'chapterVerses' : [] }
             r = m.group( 1 )
@@ -76,12 +174,13 @@ for line in f:
         if m:
             currentChapter['chapterVerses'].append( m.group( 1 ) )
 
-# last book onto analects
+# clean up
+currentBook['bookChapters'].append( currentChapter )
 analects.append( currentBook )
 f.close()
 
 f = open( 'raw-analects.yaml', 'w' )
-yaml.dump( analects, f )
+yaml.dump( analects, f, default_flow_style = False )
 f.close()
 
 
