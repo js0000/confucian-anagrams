1c9d4c8824ef8742c0056745f0b3a93417dc62ad
john saylor <jsaylor@brandeis.edu>
2017-06-28 16:10:57 -0400

    both run through pep8
    generate-yaml not quite complete
    some deprecated syntax to be fixed still

--- add-anagrams.py
+++ add-anagrams.py
@@ -5,146 +5,148 @@ import random
 import re
 import yaml
 
-## DEBUG
+# DEBUG
 import pprint
-pp = pprint.PrettyPrinter( indent = 2 )
+pp = pprint.PrettyPrinter(indent=2)
 
-## GLOBALS
+# GLOBALS
 masterCounter = 0
 
 
-## SUBROUTINES
+# SUBROUTINES
 
-# FIXME: investigate case where split occurs with punctuation immediately after stub
-def replaceStub( vt, s ):
-    verseList = re.split( s, vt, flags=re.IGNORECASE )
-    parts = len( verseList )
+# FIXME: investigate case
+# where split occurs with punctuation immediately after stub
+def replaceStub(vt, s):
+    verseList = re.split(s, vt, flags=re.IGNORECASE)
+    parts = len(verseList)
     beforeStub = []
     afterStub = []
 
     # no matches
     if parts == 0:
-        print( 'split fails for ' + vt )
+        print('split fails for ' + vt)
         return False
     elif parts > 2:
-        splitIdx = random.randint( 0, parts - 2 )
-        for i in range( parts ):
+        splitIdx = random.randint(0, parts - 2)
+        for i in range(parts):
             if i < splitIdx:
-                wordList = re.split( '\s+', verseList[i] )
-                for j in range( len( wordList ) ):
-                    if j == 0 and len( beforeStub ) < 1 :
-                        beforeStub.append( wordList[j] )
+                wordList = re.split('\s+', verseList[i])
+                for j in range(len(wordList)):
+                    if j == 0 and len(beforeStub) < 1:
+                        beforeStub.append(wordList[j])
                     else:
-                        beforeStub.append( wordList[j] )
+                        beforeStub.append(wordList[j])
                 # remove empty string from end
-                lastIdx = len( beforeStub ) - 1
+                lastIdx = len(beforeStub) - 1
                 if beforeStub[lastIdx] == '':
                     del beforeStub[lastIdx]
-                beforeStub.append( s )
+                beforeStub.append(s)
             elif i == splitIdx:
-                wordList = re.split( '\s+', verseList[i] )
-                for j in range( len( wordList ) ):
-                    beforeStub.append( wordList[j] )
+                wordList = re.split('\s+', verseList[i])
+                for j in range(len(wordList)):
+                    beforeStub.append(wordList[j])
             else:
-                wordList = re.split( '\s+', verseList[i] )
-                for j in range( len( wordList ) ):
-                    if j == 0 and len( afterStub ) < 1 :
-                        afterStub.append( wordList[j] )
+                wordList = re.split('\s+', verseList[i])
+                for j in range(len(wordList)):
+                    if j == 0 and len(afterStub) < 1:
+                        afterStub.append(wordList[j])
                     elif j == 0:
                         pass
                     else:
-                        afterStub.append( wordList[j] )
-                lastIdx = len( afterStub ) - 1
+                        afterStub.append(wordList[j])
+                lastIdx = len(afterStub) - 1
                 if afterStub[lastIdx] == '':
                     del afterStub[lastIdx]
-                afterStub.append( s )
+                afterStub.append(s)
         # remove extra stub at end
-        lastIdx = len( afterStub ) - 1
+        lastIdx = len(afterStub) - 1
         del afterStub[lastIdx]
     else:
-        beforeStub = re.split( '\s+', verseList[0] )
-        afterStub = re.split( '\s+', verseList[1] )
+        beforeStub = re.split('\s+', verseList[0])
+        afterStub = re.split('\s+', verseList[1])
 
-    ## DEBUG
-    print( 'parts: ' + str( parts ) );
-    print( 'beforeStub' )
-    pp.pprint( beforeStub )
-    print( 'stub: ' + s )
-    print( 'afterStub' )
-    pp.pprint( afterStub )
-    print( "\n" )
+    # DEBUG
+    print('parts: ' + str(parts))
+    print('beforeStub')
+    pp.pprint(beforeStub)
+    print('stub: ' + s)
+    print('afterStub')
+    pp.pprint(afterStub)
+    print("\n")
 
     return True
 
-def replaceMaster( vt, replacement ):
-    verseList = re.split( '(the master)', vt, flags=re.IGNORECASE )
+
+def replaceMaster(vt, replacement):
+    verseList = re.split('(the master)', vt, flags=re.IGNORECASE)
     processedVerse = False
-    parts = len( verseList )
+    parts = len(verseList)
 
     # no matches
     if parts == 0:
-        print( 'split fails for ' + vt )
+        print('split fails for ' + vt)
         return False
     else:
         collector = []
-        idx = random.randint( 0, parts / 2 )
+        idx = random.randint(0, parts / 2)
         # needs to be modified to work with re.split return data
         # replacement will be either only odd or only even
         idx *= 2
-        if not re.match( '^the master$', verseList[0], flags=re.IGNORECASE ):
+        if not re.match('^the master$', verseList[0], flags=re.IGNORECASE):
             if idx > 0:
                 idx -= 1
             else:
                 idx += 1
-        for i in range( parts ):
-            if len( verseList[ i ] ) < 1:
+        for i in range(parts):
+            if len(verseList[i]) < 1:
                 continue
 
-            if re.match( '^the master$', verseList[ i ], flags=re.IGNORECASE ):
-                if verseList[ i ].istitle():
+            if re.match('^the master$', verseList[i], flags=re.IGNORECASE):
+                if verseList[i].istitle():
                     if i == idx:
-                        collector.append( replacement.title() )
+                        collector.append(replacement.title())
                     else:
-                        collector.append( 'The Master' )
+                        collector.append('The Master')
                 else:
                     if i == idx:
-                        collector.append( replacement )
+                        collector.append(replacement)
                     else:
-                        collector.append( 'the Master' )
+                        collector.append('the Master')
             else:
-                collector.append( verseList[ i ] )
+                collector.append(verseList[i])
 
-    return ''.join( collector )
+    return ''.join(collector)
 
 
 # arg b is boolean
 # determining whether to cycle through options
-def selectMasterReplacement( r, b ):
+def selectMasterReplacement(r, b):
     global masterCounter
     if b:
-        replacement = r['replacementTexts']['theMaster'][ masterCounter ]
+        replacement = r['replacementTexts']['theMaster'][masterCounter]
         i = masterCounter + 1
-        moduloBase = len( r['replacementTexts']['theMaster'] )
+        moduloBase = len(r['replacementTexts']['theMaster'])
         masterCounter = i % moduloBase
     else:
-        random.shuffle( meta['replacementTexts']['theMaster'] )
-        idx = random.randint( 0, len( r['replacementTexts']['theMaster'] ) - 1 )
-        replacement = r['replacementTexts']['theMaster'][ idx ]
+        random.shuffle(meta['replacementTexts']['theMaster'])
+        idx = random.randint(0, len(r['replacementTexts']['theMaster']) - 1)
+        replacement = r['replacementTexts']['theMaster'][idx]
 
     return replacement
 
 
-def generateAnagramText( vt, r ):
-    m = re.search( 'the master', vt, flags=re.IGNORECASE )
+def generateAnagramText(vt, r):
+    m = re.search('the master', vt, flags=re.IGNORECASE)
     if m:
-        replacement = selectMasterReplacement( r, False )
-        anagramVerse = replaceMaster( vt, replacement )
+        replacement = selectMasterReplacement(r, False)
+        anagramVerse = replaceMaster(vt, replacement)
         return anagramVerse
 
     for s in r['replacementTexts']['stubs']:
-        m = re.search( s, vt, flags=re.IGNORECASE )
+        m = re.search(s, vt, flags=re.IGNORECASE)
         if m:
-            anagramVerse = replaceStub( vt, s )
+            anagramVerse = replaceStub(vt, s)
             return 'c'
 
     # FIXME: this is the random text replacement
@@ -155,33 +157,34 @@ def generateAnagramText( vt, r ):
 
 # process each verseText using regex
 # in case of multiple matches in a string, choose only one to replace
-## if 'the Master' exists directly replace
-## if one of computed list
-### find string in text and add words on each side until > N length
-### do not cross punctuation boundaries
-### shuffle anagram words and replace
-## else
-### select anagram text by looking for long words
-### continue as if found long word was computed list string and further lengthen as necessary
+#  if 'the Master' exists directly replace
+#  if one of computed list
+#   find string in text and add words on each side until > N length
+#   do not cross punctuation boundaries
+#   shuffle anagram words and replace
+#  else
+#   select anagram text by looking for long words
+#   continue as if found long word was computed list string
+#    and further lengthen as necessary
 # if original text has initial uppercase letter, do the same for replacement
 
 
-## MAIN
+# MAIN
 
-f = open( 'meta.yaml' )
-meta = yaml.load( f )
+f = open('meta.yaml')
+meta = yaml.load(f)
 f.close()
 
-f = open( 'raw-analects.yaml' )
-analects = yaml.load( f )
+f = open('raw-analects.yaml')
+analects = yaml.load(f)
 f.close()
 
 for book in analects:
     for chapter in book['bookChapters']:
         for verse in chapter['chapterVerses']:
-            anagramText = generateAnagramText( verse['verseText'], meta )
+            anagramText = generateAnagramText(verse['verseText'], meta)
 
 # add 'anagramText' key to verse object
 # add 'info' key with date/time of replacement
-## also maybe other interesting stats
+#  also maybe other interesting stats
 # dump updated data to yaml
--- generate-yaml.py
+++ generate-yaml.py
@@ -8,181 +8,187 @@ import pprint
 import re
 import yaml
 
-## ROMAN INT CONVERSION
+# ROMAN INT CONVERSION
 # from http://code.activestate.com/recipes/81611-roman-numerals/
 
+
 def int_to_roman(input):
-   """
-   Convert an integer to Roman numerals.
+    """
+    Convert an integer to Roman numerals.
 
-   Examples:
-   >>> int_to_roman(0)
-   Traceback (most recent call last):
-   ValueError: Argument must be between 1 and 3999
+    Examples:
+    >>> int_to_roman(0)
+    Traceback (most recent call last):
+    ValueError: Argument must be between 1 and 3999
 
-   >>> int_to_roman(-1)
-   Traceback (most recent call last):
-   ValueError: Argument must be between 1 and 3999
+    >>> int_to_roman(-1)
+    Traceback (most recent call last):
+    ValueError: Argument must be between 1 and 3999
 
-   >>> int_to_roman(1.5)
-   Traceback (most recent call last):
-   TypeError: expected integer, got <type 'float'>
+    >>> int_to_roman(1.5)
+    Traceback (most recent call last):
+    TypeError: expected integer, got <type 'float'>
 
-   >>> for i in range(1, 21): print int_to_roman(i)
-   ...
-   I
-   II
-   III
-   IV
-   V
-   VI
-   VII
-   VIII
-   IX
-   X
-   XI
-   XII
-   XIII
-   XIV
-   XV
-   XVI
-   XVII
-   XVIII
-   XIX
-   XX
-   >>> print int_to_roman(2000)
-   MM
-   >>> print int_to_roman(1999)
-   MCMXCIX
-   """
-   if type(input) != type(1):
-      raise TypeError, "expected integer, got %s" % type(input)
-   if not 0 < input < 4000:
-      raise ValueError, "Argument must be between 1 and 3999"   
-   ints = (1000, 900,  500, 400, 100,  90, 50,  40, 10,  9,   5,  4,   1)
-   nums = ('M',  'CM', 'D', 'CD','C', 'XC','L','XL','X','IX','V','IV','I')
-   result = ""
-   for i in range(len(ints)):
-      count = int(input / ints[i])
-      result += nums[i] * count
-      input -= ints[i] * count
-   return result
-
+    >>> for i in range(1, 21): print int_to_roman(i)
+    ...
+    I
+    II
+    III
+    IV
+    V
+    VI
+    VII
+    VIII
+    IX
+    X
+    XI
+    XII
+    XIII
+    XIV
+    XV
+    XVI
+    XVII
+    XVIII
+    XIX
+    XX
+    >>> print int_to_roman(2000)
+    MM
+    >>> print int_to_roman(1999)
+    MCMXCIX
+    """
+    if type(input) != type(1):
+        raise TypeError, "expected integer, got %s" % type(input)
+    if not 0 < input < 4000:
+        raise ValueError, "Argument must be between 1 and 3999"
+    ints = (1000, 900,  500, 400, 100,  90, 50,  40, 10,  9,   5,  4,   1)
+    nums = ('M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL',
+            'X', 'IX', 'V', 'IV', 'I')
+    result = ""
+    for i in range(len(ints)):
+        count = int(input / ints[i])
+        result += nums[i] * count
+        input -= ints[i] * count
+    return result
 
 
 def roman_to_int(input):
-   """
-   Convert a roman numeral to an integer.
-   
-   >>> r = range(1, 4000)
-   >>> nums = [int_to_roman(i) for i in r]
-   >>> ints = [roman_to_int(n) for n in nums]
-   >>> print r == ints
-   1
+    """
+    Convert a roman numeral to an integer.
 
-   >>> roman_to_int('VVVIV')
-   Traceback (most recent call last):
-    ...
-   ValueError: input is not a valid roman numeral: VVVIV
-   >>> roman_to_int(1)
-   Traceback (most recent call last):
-    ...
-   TypeError: expected string, got <type 'int'>
-   >>> roman_to_int('a')
-   Traceback (most recent call last):
-    ...
-   ValueError: input is not a valid roman numeral: A
-   >>> roman_to_int('IL')
-   Traceback (most recent call last):
-    ...
-   ValueError: input is not a valid roman numeral: IL
-   """
-   if type(input) != type(""):
-      raise TypeError, "expected string, got %s" % type(input)
-   input = input.upper()
-   nums = ['M', 'D', 'C', 'L', 'X', 'V', 'I']
-   ints = [1000, 500, 100, 50,  10,  5,   1]
-   places = []
-   for c in input:
-      if not c in nums:
-         raise ValueError, "input is not a valid roman numeral: %s" % input
-   for i in range(len(input)):
-      c = input[i]
-      value = ints[nums.index(c)]
-      # If the next place holds a larger number, this value is negative.
-      try:
-         nextvalue = ints[nums.index(input[i +1])]
-         if nextvalue > value:
-            value *= -1
-      except IndexError:
-         # there is no next place.
-         pass
-      places.append(value)
-   sum = 0
-   for n in places: sum += n
-   # Easiest test for validity...
-   if int_to_roman(sum) == input:
-      return sum
-   else:
-      raise ValueError, 'input is not a valid roman numeral: %s' % input
+    >>> r = range(1, 4000)
+    >>> nums = [int_to_roman(i) for i in r]
+    >>> ints = [roman_to_int(n) for n in nums]
+    >>> print r == ints
+    1
+
+    >>> roman_to_int('VVVIV')
+    Traceback (most recent call last):
+     ...
+    ValueError: input is not a valid roman numeral: VVVIV
+    >>> roman_to_int(1)
+    Traceback (most recent call last):
+     ...
+    TypeError: expected string, got <type 'int'>
+    >>> roman_to_int('a')
+    Traceback (most recent call last):
+     ...
+    ValueError: input is not a valid roman numeral: A
+    >>> roman_to_int('IL')
+    Traceback (most recent call last):
+     ...
+    ValueError: input is not a valid roman numeral: IL
+    """
+    if type(input) != type(""):
+        raise TypeError, "expected string, got %s" % type(input)
+    input = input.upper()
+    nums = ['M', 'D', 'C', 'L', 'X', 'V', 'I']
+    ints = [1000, 500, 100, 50,  10,  5,   1]
+    places = []
+    for c in input:
+        if not c in nums:
+            raise ValueError, "input is not a valid roman numeral: %s" % input
+    for i in range(len(input)):
+        c = input[i]
+        value = ints[nums.index(c)]
+        # If the next place holds a larger number, this value is negative.
+        try:
+            nextvalue = ints[nums.index(input[i + 1])]
+            if nextvalue > value:
+                value *= -1
+        except IndexError:
+            # there is no next place.
+            pass
+        places.append(value)
+    sum = 0
+    for n in places:
+        sum += n
+    # Easiest test for validity...
+    if int_to_roman(sum) == input:
+        return sum
+    else:
+        raise ValueError, 'input is not a valid roman numeral: %s' % input
 
 
-## MAIN
-f = open( 'raw-analects.txt', 'r' )
+# MAIN
+f = open('raw-analects.txt', 'r')
 
-pp = pprint.PrettyPrinter( indent = 2 )
+pp = pprint.PrettyPrinter(indent=2)
 analects = []
-currentBook = { 'bookChapters' : [] }
-currentChapter = { 'chapterVerses' : [] }
+currentBook = {'bookChapters': []}
+currentChapter = {'chapterVerses': []}
 nextLineTitle = False
 
 for line in f:
 
     # skip blanks
-    m = re.match( '^\s*$', line )
+    m = re.match('^\s*$', line)
     if m:
         continue
 
     l = line.rstrip()
 
     if nextLineTitle:
-        currentBook['bookName'] = l.rstrip( '.' )
+        currentBook['bookName'] = l.rstrip('.')
         nextLineTitle = False
         continue
     else:
-        m = re.match( '^(\d+)\. (.+)$', l )
+        m = re.match('^(\d+)\. (.+)$', l)
         if m:
-            currentChapter['chapterVerses'].append( { 'verseNumber' : int( m.group( 1 ) ), 'verseText' : m.group( 2 ) } )
+            verseDict = {}
+            verseDict['verseNumber'] = int(m.group(1))
+            verseDict['verseText'] = m.group(2)
+            currentChapter['chapterVerses'].append(verseDict)
 
-        m = re.match( '^BOOK (\w+)\.', l )
+        m = re.match('^BOOK (\w+)\.', l)
         if m:
             if 'bookNumber' in currentBook:
                 # clean up old chapter
-                currentBook['bookChapters'].append( currentChapter )
-                currentChapter = { 'chapterVerses' : [] }
-                analects.append( currentBook )
-                currentBook = { 'bookChapters' : [] }
-            r = m.group( 1 )
-            currentBook['bookNumber'] = roman_to_int( r )
+                currentBook['bookChapters'].append(currentChapter)
+                currentChapter = {'chapterVerses': []}
+                analects.append(currentBook)
+                currentBook = {'bookChapters': []}
+            r = m.group(1)
+            currentBook['bookNumber'] = roman_to_int(r)
             nextLineTitle = True
             continue
 
-        m = re.match( '^CHAP\. (\w+)\.', l )
+        m = re.match('^CHAP\. (\w+)\.', l)
         if m:
             if 'chapterNumber' in currentChapter:
-                currentBook['bookChapters'].append( currentChapter )
-                currentChapter = { 'chapterVerses' : [] }
-            r = m.group( 1 )
-            currentChapter['chapterNumber'] =  roman_to_int( r )
+                currentBook['bookChapters'].append(currentChapter)
+                currentChapter = {'chapterVerses': []}
+            r = m.group(1)
+            currentChapter['chapterNumber'] = roman_to_int(r)
             continue
 
 # clean up
-currentBook['bookChapters'].append( currentChapter )
-analects.append( currentBook )
+currentBook['bookChapters'].append(currentChapter)
+analects.append(currentBook)
 f.close()
 
-f = open( 'raw-analects.yaml', 'w' )
-yaml.dump( analects, f, default_flow_style = False, width = 9999, explicit_start = True )
+f = open('raw-analects.yaml', 'w')
+yaml.dump(analects, f,
+          default_flow_style=False, width=9999, explicit_start=True)
 f.close()
 
 
